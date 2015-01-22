bd42951ef8fd89f00344282bcd71c1fbb5820b04
AliZheng <aliama0288@gmail.com>
2015-01-22 14:14:53 +0800

    new added

---
+++ hello3test.java
@@ -0,0 +1,15 @@
+package hello3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class hello3test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		assertEquals(1,money.result(1));
+	}
+
+}
---
+++ money.java
@@ -0,0 +1,10 @@
+package hello3;
+
+public class money {
+	public static int result(int i)
+	{
+		return i;
+		
+	}
+
+}
