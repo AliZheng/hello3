f47ffa9302f7200b7a4c1f8593ef52d20c34bd28
AliZheng <aliama0288@gmail.com>
2015-01-22 14:24:01 +0800

    move failed message

--- hello3test.java
+++ hello3test.java
@@ -8,8 +8,7 @@ public class hello3test {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 		assertEquals(1,money.result(1));
 	}
-
 }
--- money.java
+++ money.java
@@ -6,5 +6,4 @@ public class money {
 		return i;
 		
 	}
-
 }
